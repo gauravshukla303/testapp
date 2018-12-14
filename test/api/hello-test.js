9a09c5105f726df07a57a5e195baf3df1d87666d
Gaurav <gauravshukla303@gmail.com>
2018-12-15 04:52:40 +0530

    updated tests

--- test/api/hello-test.js
+++ test/api/hello-test.js
@@ -8,19 +8,19 @@ describe('tests for /hello', function() {
     describe('tests for get', function() {
         it('should respond 200 for "Success"', function() {
             var response = request('get', 'http://localhost:10010//hello', { 
-                'qs': {"name":"ullamco nisi"},
+                'qs': {"name":"con"},
                 'headers': {"Content-Type":"application/json","Accept":"application/json"},
                 'time': true
             });
 
-            expect(response).to.have.status(200)
+        
             expect(response).to.have.schema({"required":["message"],"properties":{"message":{"type":"string"}}});
             return chakram.wait();
         });
 
         it('should respond default for "Error"', function() {
             var response = request('get', 'http://localhost:10010//hello', { 
-                'qs': {"name":"cillum consectetur tempor commodo"},
+                'qs': {"name":"occaecat Ut fugiat consectetur"},
                 'headers': {"Content-Type":"application/json","Accept":"application/json"},
                 'time': true
             });
